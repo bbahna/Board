3214a41bcd8b272cb7797bcce77fc0e6ff1b6db7
bbahna <moondomi_@naver.com>
2022-05-14 23:11:10 +0900

    popup, modal 삭제

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Main from './pages/main';
 import Sub from './pages/sub';
-import Popup from './pages/popup';
 import './index.scss';
 
 ReactDOM.render(
@@ -11,7 +10,6 @@ ReactDOM.render(
     <Routes>
       <Route path="/" element={<Main />} />
       <Route path="/sub" element={<Sub />} />
-      <Route path="/popup" element={<Popup />} />
     </Routes>
   </BrowserRouter>,
   document.getElementById('root')
--- src/pages/main/main.tsx
+++ src/pages/main/main.tsx
@@ -3,7 +3,7 @@ import './main.scss';
 import Template from '../../component/template/template';
 
 function MainPage() {
-  // Modal-button Click시, useState 상태변경
+  // Modal-button Click시, State 변경
   const [ModalShow, setModalShow] = useState(true);
   const toggleModalShow = () => setModalShow(!ModalShow);
 
@@ -14,19 +14,6 @@ function MainPage() {
           <section className='pageExplain'>
             1. 메인 페이지<br/>
             - 게시판 글 목록
-          </section>
-          <section className='madalView'>
-            {/* Modal-button */}
-            <button className='madalBtn' onClick={toggleModalShow}>
-              Modal view
-            </button>
-            {/* Modal-contents */}
-            <div className={`madalWrap ${ModalShow ? 'close' : 'open'}`}>
-              <div className='outArea' onClick={toggleModalShow}/>
-              <div className='modalBox'>
-                당신은 Modal을 발견했습니다.
-              </div>
-            </div>
           </section>
         </body>
       </Template>
